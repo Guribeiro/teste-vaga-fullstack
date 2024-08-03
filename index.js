b3a667be5513adaa8a381cfc8e766c198b2e4857
Gustavo Henrique <gugahribeiro@hotmail.com>
2024-08-03 14:41:43 -0300

    chore: replace small.csv to data.csv

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import csv from 'csv-parser'
 import { validators } from './utils/validators/index.js'
 import { transformToCurrency } from './utils/transformers/transform_to_currency.js'
 
-const readableStream = fs.createReadStream('small.csv')
+const readableStream = fs.createReadStream('data.csv')
 
 const parseFile = csv()
 
@@ -15,4 +15,4 @@ readableStream
   .pipe(transformToCurrency)
   .on('data', (chunck) => items.push(JSON.parse(chunck)))
   .on('error', (error) => console.log({error}))
-  .on('finish', () => console.log(items))
+  .on('finish', () => console.log('stream has been finished'))
